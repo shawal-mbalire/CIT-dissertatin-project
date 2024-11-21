468f405afab23b5223c898df03a197723d72dadf
Shawal Mbalire <mbalireshawal@gmail.com>
2024-11-21 17:48:30 +0300

    Update settings.py

--- HealthAppDjango/HealthAppDjango/settings.py
+++ HealthAppDjango/HealthAppDjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6@3t57v%adx#l$07i(6u)u@zz^55v1-^j-*30_pyxzkw-hsy)l"
+# SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
